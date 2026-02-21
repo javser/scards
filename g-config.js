40bbd8a312c09574206c88c49d275a172cfc7b1e
Serg <34738549+javser@users.noreply.github.com>
2026-02-21 20:35:52 +0300

    Create g-config.js

---
+++ g-config.js
@@ -0,0 +1,17 @@
+window.GameConfig = {
+    id: 'cards',
+    name: 'Карточки',
+    version: '1.0.1',
+    
+    grid: { rows: 4, cols: 4 },
+    pairs: 8,
+    cardValues: [1, 2, 3, 4, 5, 6, 7, 8],
+    
+    flipDuration: 600,
+    matchDelay: 1000,
+    
+    assets: {
+        cards: './assets/cards/',
+        sounds: './assets/sounds/'
+    }
+};
